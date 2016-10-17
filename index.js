a6e42e1ce60165a96e56c743ba5313c7931dad01
Shabanov Boris <b.shabanov@rambler-co.ru>
2016-10-17 12:11:07 +0300

    add src into block

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var os = require('os');
 var fs = require('fs');
+var path = require('path');
 var crypto = require('crypto');
-var util = require('util');
 var path = require('path');
 var childProcess = require('child_process');
 var Entities = require('html-entities').XmlEntities;
@@ -10,58 +10,71 @@ var marked = require('marked');
 var PLANTUML_JAR = path.join(__dirname, 'vendor/plantuml.jar');
 
 var entities = new Entities();
+var pagePath = {};
 
 function hashedImageName(content) {
-  var md5sum = crypto.createHash('md5');
-  md5sum.update(content);
-
-  return md5sum.digest('hex')
+    var md5sum = crypto.createHash('md5');
+    md5sum.update(content);
+    return md5sum.digest('hex');
 }
 
-function parseUmlText(sourceText) {
-  var umlText = entities.decode(sourceText).replace(/(^[ \t]*\n)/gm, '');
-  umlText = marked(umlText).replace(/^<p>/, '').replace(/<\/p>\n$/, '');
-  umlText = entities.decode(umlText);
+function parseUmlText(block, pagePath, log) {
+    var umlPath = '';
+    var umlText = '';
 
-  return umlText;
+    if (block.kwargs.src) {
+        umlPath = path.join(pagePath.dir, block.kwargs.src);
+        log.info('get plantUML content from path', umlPath);
+        umlText = fs.readFileSync(umlPath, 'utf8');
+    } else {
+        log.info('get plantUML content from context');
+        umlText = entities.decode(block.body).replace(/(^[ \t]*\n)/gm, '');
+        umlText = marked(umlText).replace(/^<p>/, '').replace(/<\/p>\n$/, '');
+        umlText = entities.decode(umlText);
+    }
+
+    return umlText;
 }
 
 module.exports = {
-  blocks: {
-    plantuml: {
-      process: function (block) {
-        var umlText = parseUmlText(block.body);
-
-        var imageName = hashedImageName(umlText) + ".png";
-        this.log.debug("using tempDir ", os.tmpdir());
-        var imagePath = path.join(os.tmpdir(), imageName);
-
-        if (fs.existsSync(imagePath)) {
-          this.log.info("skipping plantUML image for ", imageName);
+    hooks: {
+        'page:before': function (page) {
+            pagePath = path.parse(page.rawPath);
         }
-        else {
-          this.log.info("rendering plantUML image to ", imageName);
+    },
+    blocks: {
+        plantuml: {
+            process: function (block) {
+                var umlText = parseUmlText(block, pagePath, this.log);
+                var imageName = hashedImageName(umlText) + '.png';
+                var imagePath = path.join(os.tmpdir(), imageName);
+                var cwd = cwd || process.cwd();
 
-          var cwd = cwd || process.cwd();
+                this.log.debug('using tempDir ', os.tmpdir());
 
-          childProcess.spawnSync("java", [
-              '-Dplantuml.include.path=' + cwd,
-              '-Djava.awt.headless=true',
-              '-jar', PLANTUML_JAR,
-              '-pipe'
-            ],
-            {
-              // TODO: Extract stdout to a var and persist with this.output.writeFile
-              stdio: ['pipe', fs.openSync(imagePath, 'w'), 'pipe'],
-              input: umlText
-            });
+                if (fs.existsSync(imagePath)) {
+                    this.log.info('skipping plantUML image for ', imageName);
+                } else {
+                    this.log.info('rendering plantUML image to ', imageName);
+                    childProcess.spawnSync('java', [
+                            '-Dplantuml.include.path=' + cwd,
+                            '-Djava.awt.headless=true',
+                            '-jar', PLANTUML_JAR,
+                            '-pipe'
+                        ],
+                        {
+                            // TODO: Extract stdout to a var and persist with this.output.writeFile
+                            stdio: ['pipe', fs.openSync(imagePath, 'w'), 'pipe'],
+                            input: umlText
+                        });
+                }
+
+                this.log.debug('copying plantUML from tempDir for ', imageName);
+
+                this.output.copyFile(imagePath, imageName);
+
+                return '<img src="' + path.join('/', imageName) + '"/>';
+            }
         }
-        
-        this.log.debug("copying plantUML from tempDir for ", imageName);
-        this.output.copyFile(imagePath,imageName);
-
-        return "<img src=\"" + path.join("/", imageName) + "\"/>";
-      }
     }
-  }
 };
